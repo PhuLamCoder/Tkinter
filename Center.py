e666a13ed9c3a13d3a8cf7911a4603929de755e0
Trần Mỹ Phú Lâm <87407895+PhuLamCoder@users.noreply.github.com>
2022-05-27 21:29:55 +0700

    Update Center.py

--- Center.py
+++ Center.py
@@ -5,6 +5,3 @@ def center(root):
     x = (root.winfo_screenwidth() // 2) - (width // 2)
     y = (root.winfo_screenheight() // 2) - (height // 2)
     root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
-
-# Here are comments
-
