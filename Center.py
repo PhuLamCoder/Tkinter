ad00751b1f4755461f8cf40ad970df038acf6b0d
Trần Mỹ Phú Lâm <tranmyphulam123@gmail.com>
2022-05-27 21:19:12 +0700

    Quick update
    Test github desktop

--- Center.py
+++ Center.py
@@ -1,7 +1,10 @@
-def center(root) :
+def center(root):
     root.update_idletasks()
     width = root.winfo_width()
     height = root.winfo_height()
-    x = (root.winfo_screenwidth()//2) - (width//2)
-    y = (root.winfo_screenheight()//2) - (height//2)
+    x = (root.winfo_screenwidth() // 2) - (width // 2)
+    y = (root.winfo_screenheight() // 2) - (height // 2)
     root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+# Here are comments
+
